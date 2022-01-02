354b125106e1c3d4da0459b0b4bb29f8aea1a289
fariba rashtbar <f.rashtbar@mabnacloud.com>
2022-01-02 17:57:48 +0330

    for

--- js/script.js
+++ js/script.js
@@ -1,11 +1,58 @@
 'use strict'
-console.log("hello")
+// // console.log("hello")
 
 
-var num=5
-console.log(num)
+// var num=4
+// // console.log(num)
 
-var Num=[4,"fariba",true]
-var x=6
-console.log(Num)
-console.log(x)
+// // var Num=[4,"fariba",true]
+// // var x=6
+// // console.log(Num)
+// // console.log(x)
+
+// switch (num) {
+//     case 3:
+//     case 4:
+//         console.log("print 3+")
+//         break;
+//     // case 4:
+//     //     console.log("print 4")
+//     //     break;
+//     case 5:
+//         console.log("print 5")
+//         break;
+//     default:
+//         console.log("wrong")
+//         break;
+// }
+
+// switch (true) {
+//     case (num==4 || num==3):
+//         console.log("print 3+")
+//         break;
+//     // case 4:
+//     //     console.log("print 4")
+//     //     break;
+//     case (num==5):
+//         console.log("print 5")
+//         break;
+//     default:
+//         console.log("wrong")
+//         break;
+// }
+var items=[1,2,3,4,5,6,75]
+function name(items) {
+    
+
+
+// items.forEach(function(item) {
+//     console.log(item);
+// });
+
+return items.reduce(function(total,value) {
+   console.log(total,value) 
+// return total,value
+});
+}
+
+name(items);
