8bcf298a2e59f80c8f662a3336603d83b20cf93c
fariba rashtbar <f.rashtbar@mabnacloud.com>
2022-01-12 16:25:52 +0330

    Object and Prototype

---
+++ js/objectScript.js
@@ -0,0 +1,66 @@
+'use strict'
+var car={
+    make:'ford',
+    moddel:'munstag',
+    year:1996,
+    "year-old":1900,
+    detail:{
+        color:['red','black','blue'],
+        wheels:4
+    },
+    summery:function(){
+        return this.moddel+' '+this.year
+    }
+}
+console.log(car.make)
+car.make='saipa'
+console.log(car.make)
+car.name='irankhodro'
+console.log(car)
+car["year-old"]=1800
+console.log(car["year-old"])
+console.log(car.summery())
+
+var car_2=new Object()
+car_2.name='dena'
+car_2.year=1400
+console.log(car_2)
+
+//------------constructor--------------
+// function Car(){
+// this.make="ford"
+//  this.run=function(){
+//     return "Speed up to 200";
+// }
+// }
+
+// var car_3=new Car();
+// // car_3.moddel="ford"
+
+function Car(make,model,year){
+this.make=make;
+this.model=model;
+this.year=year;
+this.run=function(){
+    return "Speed Of to 200";
+}
+}
+Car.prototype.ww=function(){
+    return "fun";
+}
+var car_3=new Car("saipa","pride",1990)
+var car_4=new Car("saipa","pride",1990)
+car_4.color="blue"
+car_4.makeSmile=function(){
+    console.log(":)")
+}
+
+var car_5=Object.create(null);
+car_5.sayHello=function(){
+    console.log("hello!");
+}
+var car_6=Object.create(car_4);
+car_6.makeSmile=function(){
+    console.log(":(");
+}
+var car_7=Object.create(car_5);
--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@
 //         break;
 // }
 var items=[1,2,3,4,5,6,75]
-function name(items) {
+function sum(items) {
     
 
 
@@ -55,4 +55,35 @@ return items.reduce(function(total,value) {
 });
 }
 
-name(items);
+console.log(sum(items));
+
+
+///////arrey
+var list=['item',1,8,6,7,52,33]
+var list2=[19,66]
+// console.log(list[3]);
+// console.log(list.push(13))
+// console.log(list);
+// console.log(list.pop())
+// console.log(list);
+// console.log(list.indexOf(52))
+// console.log(list[list.indexOf(33)])
+// console.log(list.shift())
+// console.log(list.unshift('fariba'))
+// console.log(list.splice(1,3))
+// console.log(list.slice(2,5))
+// console.log(list.join())
+// console.log(list);
+// console.log(list.join('-'))
+// console.log(list);
+// console.log(list.concat(list2))
+// console.log(list.sort())
+// console.log(list.reverse())
+// var filterList=list.filter(function(item){
+//    return item%2==1
+// })
+// console.log(filterList)
+// var mapList=list.map(function(item){
+//    return item+3
+// })
+// console.log(mapList)
