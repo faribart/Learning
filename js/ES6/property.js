947204fe0fe422aa186866be8971fadabac5a7a7
fariba rashtbar <f.rashtbar@mabnacloud.com>
2022-02-28 17:54:07 +0330

    es6-2

--- js/ES6/property.js
+++ js/ES6/property.js
@@ -1,17 +1,129 @@
-'use strict'
 
-function func(){
-var x=10;
-if(true){
-    x='fariba'
+//let
+// function func(){
+// let x=10;
+// if(true){
+//    let x='fariba'
+//    console.log(x)
+// }
+// console.log(x);
+// }
+
+// func()
+
+// var array=[1,2,3,4];
+// for(var index=0;index<=array.length;index++){
+
+// }
+// console.log(index);
+
+
+//const
+
+// const x=4;
+// x=3
+// console.log(x)
+
+// const x=[1,5,6]
+
+// x.push(4)
+
+// console.log(x)
+
+// const NAME={age:23}
+// // NAME={age:25}
+// NAME.age=20
+// console.log(NAME)
+
+
+//arrow
+
+// let sum=(a,b) => a+b
+//return object
+// let sum=(a,b) => ({a:a,b:b})
+//or
+// let sum=(a,b) => ({a,b})
+// console.log(sum(4,8))
+// let arr=[3,6,2]
+// console.log(arr.map(function(item){
+//     return item*3
+// }))
+
+// console.log(arr.map((item) => item*3))
+// console.log(arr.map(item => item*3))
+
+// function person(){
+//     this.age=20
+// // let that=this;
+//     setInterval(() => {
+//         this.age++
+//         console.log(this.age)
+//     }, 1000);
+// }
+
+// let pPerson=new person()
+// let arguments=[1,3,8,9]
+
+// function sum(a,b){
+//     console.log(arguments)
+// }
+// let sum=(a,b) =>console.log(arguments)
+
+// sum(1,5)
+// console.log(sum(3,6))
+
+// let obj={
+//     a:10,
+//     b:function(){
+//         console.log(this.a,this)
+//     },
+//     c:() =>{
+//         console.log(this.a,this)
+//     }
+// }
+
+
+// obj.b()
+// obj.c()
+
+//default parameter
+
+// function sum(x,y){
+// // let a=typeof x==='undefined'?1:x
+// let a=x||1;
+// // let b=typeof y==='undefined'?1:y
+// let b=y||1;
+// console.log(a,b)
+// }
+
+// function sum(a=1,b=1){
+//     console.log(a,b)
+// }
+
+
+// sum()
+
+
+//rest
+
+// function sum(...c){
+//     return c.reduce((pre,current)=>{
+//         return pre+current
+//     })
+// }
+
+
+// console.log(sum(1,2,3,4,5,6,8,9))
+
+
+//spread
+
+function sum(a,b,c){
+    console.log(a+b+c)
 }
-console.log(x);
-}
 
-func()
+let list=[1,2,3]
 
-var array=[1,2,3,4];
-for(var index=0;index<=array.length;index++){
+// sum.apply(null,list)
 
-}
-console.log(index);
+sum(...list)
